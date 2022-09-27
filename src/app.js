5df94d4c2084a2a6ec09be89410762ecd45fc9d2
felipe29cl <fcarrascoa29@gmail.com>
2022-09-27 18:46:16 +0000

    Generator Domain

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".cl", ".com", ".org"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let generatorName = [];
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < domain.length; d++) {
+          let generatorDomain = pronoun[a] + adj[b] + noun[c] + domain[d];
+          generatorName.push(generatorDomain);
+        }
+      }
+    }
+  }
+
+  for (var i = 0; i < generatorName.length; i++) {
+    console.log(generatorName[i]);
+  }
 };
